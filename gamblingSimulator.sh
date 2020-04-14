87fa0eec811b1474c5ecd3c80d6e63bd4e00bc71
DipaliBachhav <dipa2718@gmail.com>
2020-04-14 11:47:57 +0530

    Added Luck and Unlucky Day

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -35,6 +35,46 @@ do
 		fi
 	done
 	(( day++ ))
+
+#calculated total win amount ,loss amount and day win or loss
+total_win_amount=$(($total_win_amount+$win_Amount))
+total_loose_amount=$(($total_loose_amount+$loose_Amount))
+
+if [ $total_win_amount -gt $total_loose_amount ]
+then
+	 result1=$(($total_win_amount-$total_loose_amount))
+	 ((win_day++))
+else
+	result1=$(($total_loose_amount-$total_win_amount))
+	((loss_day++))
+fi
+array1[$day]=$win_Amount
+array2[$day]=$loose_Amount
+((day++))
 done
-echo "Win Amount := $win_Amount"
-echo "Loose Amount := $loose_Amount"
+#calculated luckiest day
+echo " ${array1[@]}"
+max_value=${array1[1]}
+for (( i=1;i<=20;i++ ))
+do
+	if [[ ${array1[$i]} -gt $max_value ]]
+	then
+			max_value=${array1[$i]}
+			luckiest_day=$i
+	fi
+done
+echo "Win Amount $win_Amount"
+echo "Loose Amount $loose_Amount"
+
+#calculated unluckiest day
+echo "${array2[@]}"
+min_value=${array2[1]}
+for (( j=1;j<=20;j++ ))
+do
+	if [[ ${array2[$j]} -lt $min_value ]]
+	then
+		min_value=${array1[$j]}
+		unluckiest_day=$j
+	fi
+done
+
